27c21dfe8c992d697e6b7c95a02b75b79549b33e
roy <roy.cao@hotmail.com>
2021-05-31 19:30:33 +0800

    add create api

---
+++ src/main/java/com/meet2025/dataring/domain/DataCatalogRepository.java
@@ -0,0 +1,4 @@
+package com.meet2025.dataring.domain;/**
+ * @author Roy Cao
+ */public class DataCatalogRepository {
+}
---
+++ src/test/java/com/meet2025/dataring/CachingTests.java
@@ -0,0 +1,4 @@
+package com.meet2025.dataring;/**
+ * @author Roy Cao
+ */public class CachingTests {
+}
